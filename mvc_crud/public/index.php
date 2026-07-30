d13f662c7b969c02c449f423474da2a9b5614ff4
Antonella11R <rendonantonella26@gmail.com>
2026-07-30 16:12:27 -0500

    actualizado

---
+++ mvc_crud/public/index.php
@@ -0,0 +1,38 @@
+//*front controllers*//
+//*punto de entrada al canal*//
+<?php
+require_once __DIR__ . "/../app/controllers/UsuarioControllers.php";
+
+$url = $_GET["url"] ?? "usuarios/listar";
+
+$controller = new UsuarioController();
+
+switch ($url) {
+    case "usuarios/listar":
+        $controller->listar();
+        break;
+
+    case "usuarios/crearForm":
+        $controller->crearForm();
+        break;
+
+    case "usuarios/crear":
+        $controller->crear();
+        break;
+
+    case "usuarios/editarForm":
+        $controller->editarForm();
+        break;
+
+    case "usuarios/actualizar":
+        $controller->actualizar();
+        break;
+
+    case "usuarios/eliminar":
+        $controller->eliminar();
+        break;
+
+    default:
+        http_response_code(404);
+        echo "404 - Ruta no encontrada";
+}
